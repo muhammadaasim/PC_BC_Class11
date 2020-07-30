8b78f430b0b97d66dfd98a5d6a354bae44639b83
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-07-30 21:25:49 +0500

    Update RouteConfig.js

--- src/RouteConfig.js
+++ src/RouteConfig.js
@@ -1,5 +1,5 @@
 import React from 'react';
- // import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import Product from './Components/Product';
@@ -12,14 +12,14 @@ function RouteConfig() {
     return (
         <div>
             <Router>
-                <Navbar/>
+                <Navbar />
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route path="/about" component={About} />
                     <Route exact path="/product" component={Product} />
                     <Route path="/product/:id" component={ProductItem} />
-                    <Route path="*" component={NF} />
-                    </Switch>
+                    <Route path="*" component={NF}/>
+                </Switch>
             </Router>
         </div>
     )
